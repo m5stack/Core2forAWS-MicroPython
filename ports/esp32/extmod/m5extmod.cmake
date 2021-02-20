479c41351a35645825914cfaa66476c7bf4f9806
sorz <heycoolbin@gmail.com>
2021-02-20 10:49:13 +0800

    Add i2c_bus automatic configuration  and thread safe

--- ports/esp32/extmod/m5extmod.cmake
+++ ports/esp32/extmod/m5extmod.cmake
@@ -6,5 +6,12 @@ file(GLOB_RECURSE DISPLAY_FILE ${MICROPY_M5_EXTMOD_DIR}/tft/*.c)
 
 set(MICROPY_M5_SOURCE_EXTMOD
     ${MICROPY_M5_EXTMOD_DIR}/moddisplay.c
+    ${MICROPY_M5_EXTMOD_DIR}/i2c_bus/i2c_device.c
+    ${MICROPY_M5_EXTMOD_DIR}/machine_i2c.c
     ${DISPLAY_FILE}
 )
+
+list(APPEND MICROPY_SOURCE_EXTMOD_EXTRA ${MICROPY_M5_SOURCE_EXTMOD})
+
+# remove micropython esp32 base i2c lib
+list(REMOVE_ITEM MICROPY_SOURCE_PORT ${PROJECT_DIR}/machine_i2c.c)
