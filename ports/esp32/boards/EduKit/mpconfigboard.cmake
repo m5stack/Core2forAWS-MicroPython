c27af3a06eb93c2d2b707c36c3b06423b2380789
sorz <heycoolbin@gmail.com>
2021-03-04 15:46:55 +0800

    Add esp-cryptoauthlib support

--- ports/esp32/boards/EduKit/mpconfigboard.cmake
+++ ports/esp32/boards/EduKit/mpconfigboard.cmake
@@ -1,5 +1,6 @@
 set(SDKCONFIG_DEFAULTS
-    boards/EduKit/sdkconfig.board
+    boards/EduKit/sdkconfig
 )
 
+set(EXTRA_COMPONENT_DIRS ${MICROPY_PORT_DIR}/extmod/components)
 set(MICROPY_FROZEN_MANIFEST ${MICROPY_BOARD_DIR}/manifest.py)
